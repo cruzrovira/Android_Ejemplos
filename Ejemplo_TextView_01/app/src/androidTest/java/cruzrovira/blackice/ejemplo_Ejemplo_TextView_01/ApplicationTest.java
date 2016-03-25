efc32fb532c7ecc1de9e73b77c140b698ccd3072
cruzrovira <cruzrovira@gmail.com>
2016-03-25 12:10:08 -0600

    Realizacion de ejemplo con textView ejemplo 01

---
+++ Ejemplo_TextView_01/app/src/androidTest/java/cruzrovira/blackice/ejemplo_Ejemplo_TextView_01/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cruzrovira.blackice.ejemplo_Ejemplo_TextView_01;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
